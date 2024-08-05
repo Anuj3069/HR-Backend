b9c50b64af8b5173e076f065f1a1db207927ed61
deepak96999 <anujgupta596@gmail.com>
2024-08-06 01:38:31 +0530

    all setup configured

---
+++ src/models/users.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        contactNumber: {
+            type: String,
+            pattern: "^[0-9]{10}$"
+        },
+        resume: {   //cloudinary url
+            type: String,
+            required: true
+        },
+        message: {
+            type: String,
+          }
+
+   },
+    {
+        timestamps:true
+    }
+)
+
+
+export const User = mongoose.model("User", userSchema)
