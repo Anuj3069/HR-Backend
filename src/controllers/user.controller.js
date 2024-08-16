fae8be254082a073853c46a29fe7085016d30a41
deepak96999 <anujgupta596@gmail.com>
2024-08-16 12:41:53 +0530

    cloudinary code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/users.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import mongoose from "mongoose";
 
 
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, lastName, contactNumber } = req.body;
+    if ([username, email, lastName, contactNumber].some((field) =>
+        field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+  
+    // Check for existing email
+    // const existedUser = await User.findOne({ email });
+    // if (existedUser) {
+    //     throw new ApiError(400, "User with this email already exists");
+    // }
+  
+    const resumeLocalPath = req.files?.resume[0]?.path;
+    console.log(resumeLocalPath);
+  
+    if (!resumeLocalPath) {
+        throw new ApiError(400, "Resume file is required");
+    }
+  
+    const resume = await uploadOnCloudinary(resumeLocalPath);
+    if (!resume) {
+        throw new ApiError(500, "Error uploading resume file");
+    }
+  
+    // const user = await User.create({
+    //     username: username.toLowerCase(),
+    //     email,
+    //     lastName,
+    //     contactNumber,
+    //     resume: resume.url
+    // });
+  
+    // const createdUser = await User.findById(user._id);
+  
+    // if (!createdUser) {
+    //     throw new ApiError(500, "Something went wrong while registering the user");
+    // }
+    const user = await User.findOneAndUpdate(
+        { email },
+        {
+            username: username.toLowerCase(),
+            lastName,
+            contactNumber,
+            // resume: resume.url
+            resume: resume.display_name
+        },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
+    console.log(resume)
 
-const registerUser = asyncHandler( async (req, res) => {
-
-    res.status(200).json({
-        message:"ok"
-    })
-})
-
-export {registerUser}
+    if (!user) {
+        throw new ApiError(500, "Something went wrong while registering/updating the user");
+    }
+    // return res.status(201).json(
+    //     new ApiResponse(200, createdUser, "User added successfully")
+    // );
+    return res.status(200).json(
+        new ApiResponse(200, user, "Data registered/updated successfully")
+    );
+  });
+  export {registerUser}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,10 +2,21 @@ import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+// cloudinary.config({ 
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//   api_key: process.env.CLOUDINARY_API_KEY, 
+//   api_secret: process.env.CLOUDINARY_API_SECRET 
+// });
+cloudinary.config({
+    cloud_name: 'dfe82hk93',
+    api_key: '418797228397462',
+    api_secret: 'ciTrKLyLqptdmZ1B6MWJ2RKVHBA'
+});
+
+console.log('Cloudinary Config:', {
+    cloud_name: cloudinary.config().cloud_name,
+    api_key: cloudinary.config().api_key,
+    api_secret: cloudinary.config().api_secret
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -15,6 +26,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
+        
         // file has been uploaded successfull
         //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
